92385fe78d7c36ec4c0c5ac25c60fa96a0ce6603
xonmello <43194417+xonmello@users.noreply.github.com>
2019-06-22 01:33:58 -0700

    Added more functionality

--- run.bat
+++ run.bat
@@ -2,9 +2,14 @@
 SET "cp=%CD%"
 cd .\dll\
 START /b CMD /c RLBot_Injector.exe
+ECHO Injecting DLLS
 cd /d C:\Program Files (x86)\Steam\
 START /b CMD /c Steam.exe -applaunch 252950 -rlbot
+ECHO Starting Rocket League with -rlbot flag
 cd /d %cp%
-TIMEOUT /T 10
+ECHO
+ECHO Waiting for Rocket League to start and DLLS to be injected. Press any key when this is done or wait.
+TIMEOUT /T 30 > NUL
 cargo run
-pause
+ECHO Bot no longer running... press any key to shutdown.
+pause > NUL
