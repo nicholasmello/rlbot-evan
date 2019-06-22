b16fed30099cf209d11a0e26568ea22f05cbcb8b
xonmello <43194417+xonmello@users.noreply.github.com>
2019-06-22 02:28:26 -0700

    Working with python framework installed
    The bot is working with the python framework installed. Might add parts of the python framework into here to make it easier.

--- run.bat
+++ run.bat
@@ -1,15 +1,4 @@
 @ECHO OFF
-SET "cp=%CD%"
-cd .\dll\
-START /b CMD /c RLBot_Injector.exe
-ECHO Injecting DLLS
-cd /d C:\Program Files (x86)\Steam\
-START /b CMD /c Steam.exe -applaunch 252950 -rlbot
-ECHO Starting Rocket League with -rlbot flag
-cd /d %cp%
-ECHO
-ECHO Waiting for Rocket League to start and DLLS to be injected. Press any key when this is done or wait.
-TIMEOUT /T 30 > NUL
-cargo run
+cargo build && python -c "from rlbot import runner; runner.main()"
 ECHO Bot no longer running... press any key to shutdown.
-pause > NUL
+TIMEOUT /T 120 > NUL
